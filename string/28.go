6366a4b73ea62e7022d0ce5b5fa8d15eb4a44795
yanlonglv <lyl_office@163.com>
2020-11-11 22:08:16 +0800

    leetcode 28 strStr2

--- string/28.go
+++ string/28.go
@@ -38,3 +38,20 @@ func getNext(s []byte) []int {
 	}
 	return next
 }
+
+func strStr2(haystack string, needle string) int {
+	l1 := len(haystack)
+	l2 := len(needle)
+	if l2 == 0 {
+		return 0
+	}
+	if l1 == 0 || l1 < l2 {
+		return -1
+	}
+	for i := 0; i <= l1-l2; i++ {
+		if haystack[i:i+l2] == needle {
+			return i
+		}
+	}
+	return -1
+}
