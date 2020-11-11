4d662171050b224d0b78cc27b1a1417ec050c72c
yanlonglv <lyl_office@163.com>
2020-11-11 22:00:54 +0800

    add leetcode 28

---
+++ string/28.go
@@ -0,0 +1,40 @@
+package string
+
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	next := getNext([]byte(needle))
+	j := -1                              // 因为next数组里记录的起始位置为-1
+	for i := 0; i < len(haystack); i++ { // 注意i就从0开始
+		// 不匹配
+		for j >= 0 && haystack[i] != needle[j+1] {
+			j = next[j] // j 寻找之前匹配的位置
+		}
+		// 匹配，j和i同时向后移动
+		if haystack[i] == needle[j+1] {
+			j++
+		}
+		// 文本串s里出现了模式串t
+		if j == len(needle)-1 {
+			return i - len(needle) + 1
+		}
+	}
+	return -1
+}
+
+func getNext(s []byte) []int {
+	j := -1
+	next := make([]int, len(s))
+	next[0] = j
+	for i := 1; i < len(s); i++ { // 注意i从1开始
+		for j >= 0 && s[i] != s[j+1] {
+			j = next[j] // 向前回溯
+		}
+		if s[i] == s[j+1] { // // 找到相同的前后缀
+			j++
+		}
+		next[i] = j // // 将j（前缀的长度）赋给next[i]
+	}
+	return next
+}
