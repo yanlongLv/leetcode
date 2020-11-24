e8caca9582ad4692b3e1eaf894e058980a0ddfbb
yanlonglv <lyl_office@163.com>
2020-11-24 22:56:26 +0800

    add greed 455

---
+++ greed/455.go
@@ -0,0 +1,17 @@
+package greed
+
+import "sort"
+
+func findContentChildren(g []int, s []int) int {
+	sort.Ints(g)
+	sort.Ints(s)
+	i := 0
+	j := 0
+	for i < len(g) && j < len(s) {
+		if g[i] <= s[j] {
+			i++
+		}
+		j++
+	}
+	return i
+}
