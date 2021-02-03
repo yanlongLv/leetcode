4657109b4cf2d9a57c8beb7f572c10b60a15415d
Lv Yanlong <lvyanlong@senbit.com>
2021-02-03 14:28:29 +0800

    fixed bug about 283

--- array/283.go
+++ array/283.go
@@ -1,8 +1,10 @@
+package array
+
 func moveZeroes(nums []int) {
 	j := 0
 	for i := 0; i < len(nums); i++ {
-		if mums[i] != 0 {
-			mums[i], nums[j] = nums[j], nums[i]
+		if nums[i] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
 			j++
 		}
 	}
