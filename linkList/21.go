d5dceb5faff49f8e61a30dee1552878f900b8624
Lv Yanlong <lvyanlong@senbit.com>
2021-02-04 12:03:01 +0800

    add link list

---
+++ linkList/21.go
@@ -0,0 +1,28 @@
+package linkList
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	node := new(ListNode)
+	head := node
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			node.Next = l1
+			l1 = l1.Next
+		} else {
+			node.Next = l2
+			l2 = l2.Next
+		}
+		node = node.Next
+	}
+	if l1 == nil && l2 != nil {
+		node.Next = l2
+	} else {
+		node.Next = l1
+	}
+	return head.Next
+}
