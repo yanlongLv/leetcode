763f98bcae9b0359a2170a6c05de000636092169
Lv Yanlong <lvyanlong@senbit.com>
2021-02-05 18:06:28 +0800

    add tree 110 node

---
+++ tree/110.go
@@ -0,0 +1,38 @@
+package tree
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isBalanced(root *TreeNode) bool {
+	return height(root) >= 0
+}
+
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftHeight := height(root.Left)
+	rightHeight := height(root.Right)
+	if leftHeight == -1 || rightHeight == -1 || abs(leftHeight-rightHeight) > 1 {
+		return -1
+	}
+	return max(leftHeight, rightHeight) + 1
+}
+
+func abs(x int) int {
+	if x > 0 {
+		return x
+	}
+	return -x
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
