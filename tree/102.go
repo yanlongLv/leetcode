682c71f9d49ac6f9642587e11818b4b160ee4f3f
Lv Yanlong <lvyanlong@senbit.com>
2021-02-07 14:25:28 +0800

    tree 102 node leetcode

---
+++ tree/102.go
@@ -0,0 +1,34 @@
+package tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	result := [][]int{}
+	quene := []*TreeNode{root}
+	for len(quene) > 0 {
+		queneLength := len(quene)
+		res := []int{}
+		for i := 0; i < queneLength; i++ {
+			node := quene[i]
+			res = append(res, node.Val)
+			if node.Left != nil {
+				quene = append(quene, node.Left)
+			}
+			if node.Right != nil {
+				quene = append(quene, node.Right)
+			}
+		}
+		quene = quene[queneLength:]
+		if len(res) > 0 {
+			result = append(result, res)
+		}
+	}
+	return result
+}
