99cddd4863bcd3606bdf864b89c291221c74a597
yanlonglv <lyl_office@163.com>
2021-04-09 00:12:45 +0800

    add hash leetcode item

---
+++ hash/13.go
@@ -0,0 +1,22 @@
+func romanToInt(s string) int {
+	var roman = map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	prev, result := roman[s[0]], roman[s[0]]
+	for i := 1; i < len(s); i++ {
+		cur := roman[s[i]]
+		if cur > prev {
+			result = result - 2*prev + cur
+		} else {
+			result += cur
+		}
+		prev = cur
+	}
+	return result
+}
