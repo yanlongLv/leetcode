67f88bcefe91d6fc1f0d61ac203c78f7fc0f7c97
yanlonglv <lyl_office@163.com>
2021-04-08 23:43:26 +0800

    add skill 8 leetcode

---
+++ skills/8.go
@@ -0,0 +1,40 @@
+func myAtoi(str string) int {
+	n := len(str)
+	var i, j int
+	neg := false
+	for i = 0; i < n; i++ {
+		if str[i] >= '0' && str[i] <= '9' {
+			break
+		} else if str[i] == '+' {
+			i++
+			break
+		} else if str[i] == '-' {
+			neg = true
+			i++
+			break
+		} else if str[i] != ' ' {
+			return 0
+		}
+	}
+	for j = i; j < n; j++ {
+		if str[j] < '0' || str[j] > '9' {
+			break
+		}
+	}
+	ret := 0
+	for k := i; k < j; k++ {
+		cur := int(str[k] - '0')
+		if !neg {
+			ret = ret*10 + cur
+			if ret > int32Max {
+				return int32Max
+			}
+		} else {
+			ret = ret*10 - cur
+			if ret < int32Min {
+				return int32Min
+			}
+		}
+	}
+	return ret
+}
