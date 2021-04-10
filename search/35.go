a799dda247315043584ff59a243fede30d73ebc3
yanlonglv <lyl_office@163.com>
2021-04-10 23:26:28 +0800

    add search 35 leetcode

---
+++ search/35.go
@@ -0,0 +1,13 @@
+func searchInsert(nums []int, target int) int {
+	left := 0
+	right := len(nums)
+	for left < right {
+		middle := (right + left) / 2
+		if nums[middle] >= target {
+			right = middle
+		} else {
+			left = middle + 1
+		}
+	}
+	return right
+}
